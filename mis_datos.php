2bc870a9aeea5ceed89d0dd05fe596c343993d0b
David Ceron <ceron.david@gmail.com>
2025-12-08 16:31:51 +0000

    Primeros pasos

---
+++ mis_datos.php
@@ -0,0 +1,8 @@
+<?php
+    $nombres= "David";
+    $apellidos = "Ceron Meneses";
+    $correo = "ceron.david@gmail.com";
+    $cargo = "Ingeniero en sistemas";
+    $domicilio = "CDMX";
+
+    $nombre_completo = $nombres . " " . $apellidos;
